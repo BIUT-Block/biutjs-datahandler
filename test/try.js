2ca79fbde7d38294e3b195fb678b4029eda78dc3
Zu Weihan <zuweihan1992@gmail.com>
2019-02-26 11:05:10 +0100

    improved test file

--- test/try.js
+++ test/try.js
@@ -4,30 +4,32 @@ const config = {
   'DBPath': '../data/'
   // 'StateRoot': '2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429e3'
 }
-const root = '2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429f3'
+const root = '2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429e3'
 const accTree = new AccTreeDB(config)
+// console.log(accTree.getRoot())
 
-accTree.constructNewTree(root)
+// accTree.constructNewTree(root)
 
-accTree.putAccInfo('12345', ['1.1', 2], (err) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('success')
-    console.log(accTree.getRoot())
-  }
-})
+// accTree.putAccInfo('12345', ['1.1', 2], (err) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log('success')
+//     console.log(accTree.getRoot())
+//   }
+// })
 
 // accTree.getAccInfo('12345', (err, data) => {
 //   if (err) {
 //     console.log('===============')
 //     // console.log(err)
-//     accTree.constructNewTree(root)
+//     // accTree.constructNewTree(root)
 //     accTree.getAccInfo('12345', (err, data) => {
 //       if (err) {
+//         console.log(accTree.getRoot())
 //         console.log('2222222222')
-//         // console.log(err)
 //       } else {
+//         console.log(accTree.getRoot())
 //         console.log(data)
 //       }
 //     })
@@ -37,7 +39,7 @@ accTree.putAccInfo('12345', ['1.1', 2], (err) => {
 //   }
 // })
 
-// accTree.checkRoot('2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429e3', (err, result) => {
-//   console.log(err)
-//   console.log(result)
-// })
+accTree.checkRoot('2eb39ae30f26e27dfdbcfbc114323daa9dd2c74786974e19a775c24432d429e3', (err, result) => {
+  console.log(err)
+  console.log(result)
+})
