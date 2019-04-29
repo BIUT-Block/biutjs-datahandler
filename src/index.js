310238a782b75882c74c6cef7f3b1c6cca2969fb
Zu Weihan <zuweihan1992@gmail.com>
2019-04-29 10:03:42 +0200

    remove tx chain db

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const AccTreeDB = require('./accTreeDB')
 const TokenBlockChainDB = require('./tokenBlockChainDB')
-const TxBlockChainDB = require('./txBlockChainDB')
 const TokenTxDB = require('./tokenTxDB')
 const SmartContractTxDB = require('./SmartContractTxDB')
 
 module.exports = {
   AccTreeDB: AccTreeDB,
   TokenBlockChainDB: TokenBlockChainDB,
-  TxBlockChainDB: TxBlockChainDB,
   TokenTxDB: TokenTxDB,
   SmartContractTxDB: SmartContractTxDB
 }
