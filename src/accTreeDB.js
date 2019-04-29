41002bf6f72e6b30231d55ee04f4f5231a9d0167
Ming <ming1992liu@gmail.com>
2019-04-29 21:54:39 +0200

    fix

--- src/accTreeDB.js
+++ src/accTreeDB.js
@@ -156,7 +156,7 @@ class AccTreeDB {
     dataHandlerUtil._clearDB(this.accTreeDB, callback)
   }
 
-  getAccInfo (accAddress, callback) {
+  getAccInfo (accAddress, tokenName, callback) {
     this.tree.get(accAddress, (err, value) => {
       if (err) return callback(err)
       try {
